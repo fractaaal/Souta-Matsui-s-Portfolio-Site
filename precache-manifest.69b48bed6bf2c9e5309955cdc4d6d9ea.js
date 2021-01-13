94a12b31ebb3e8f25053be50d30f60686ddd3fa8
fractaaal <b1329180@planet.kanazawa-it.ac.jp>
2021-01-13 14:32:32 +0900

    Updates

---
+++ precache-manifest.69b48bed6bf2c9e5309955cdc4d6d9ea.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db926e1047cdacc3f7f1d0270da5b7f",
+    "url": "/Souta-Matsui-s-Portfolio-Site/index.html"
+  },
+  {
+    "revision": "d2c75837520ca900c636",
+    "url": "/Souta-Matsui-s-Portfolio-Site/static/css/main.d6c2f949.chunk.css"
+  },
+  {
+    "revision": "f7fa4cd8424dfeb77d14",
+    "url": "/Souta-Matsui-s-Portfolio-Site/static/js/2.23e336f7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Souta-Matsui-s-Portfolio-Site/static/js/2.23e336f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c75837520ca900c636",
+    "url": "/Souta-Matsui-s-Portfolio-Site/static/js/main.058cf2c4.chunk.js"
+  },
+  {
+    "revision": "958c938d16c22d99c0ae",
+    "url": "/Souta-Matsui-s-Portfolio-Site/static/js/runtime-main.76114eb0.js"
+  },
+  {
+    "revision": "8b3d4d5f2b65a3088a01e02f28ebd11a",
+    "url": "/Souta-Matsui-s-Portfolio-Site/static/media/cafeshira_picture.8b3d4d5f.jpg"
+  },
+  {
+    "revision": "0ca090704f412f9e7fabb95f423f0b5b",
+    "url": "/Souta-Matsui-s-Portfolio-Site/static/media/gurume.0ca09070.jpg"
+  },
+  {
+    "revision": "795e64c473a29f317d080ba88373cee5",
+    "url": "/Souta-Matsui-s-Portfolio-Site/static/media/kanazawa1.795e64c4.jpg"
+  },
+  {
+    "revision": "e8ef2241bf05af429bc6b8ea812defb9",
+    "url": "/Souta-Matsui-s-Portfolio-Site/static/media/machine.e8ef2241.jpg"
+  },
+  {
+    "revision": "e64eea88ae85ea484d51c270bc0e1e4e",
+    "url": "/Souta-Matsui-s-Portfolio-Site/static/media/mandelblot.e64eea88.jpg"
+  },
+  {
+    "revision": "431f2d3136e853439d0321a2bb21447d",
+    "url": "/Souta-Matsui-s-Portfolio-Site/static/media/mask.431f2d31.jpg"
+  },
+  {
+    "revision": "17d71693036c22639d0c25230ae108a3",
+    "url": "/Souta-Matsui-s-Portfolio-Site/static/media/math.17d71693.jpg"
+  },
+  {
+    "revision": "c178e52de85712d2e1126e441b56ad69",
+    "url": "/Souta-Matsui-s-Portfolio-Site/static/media/mosaic.c178e52d.jpg"
+  }
+]);
